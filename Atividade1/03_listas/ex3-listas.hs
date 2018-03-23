bc1ab0314db6e98602e01d5ec9f144074b12aea9
Eduardo Pinhata <epinhata@Eduardos-MacBook-Pro.local>
2018-03-23 09:42:36 -0300

    Update da lista

--- Atividade1/03_listas/ex3-listas.hs
+++ Atividade1/03_listas/ex3-listas.hs
@@ -6,25 +6,28 @@
 
 -- Exercício 01: Crie uma função divisivel20 x que retorna verdadeiro se x for divisível por todos os números de 1 a 20.
 divisivel20 :: Integer -> Bool
-divisivel20 x = foldl (&&) True [ (rem x i) == 0 | i <- [1..5]]
+divisivel20 x = foldl (&&) True [ (rem x i) == 0 | i <- [1..20]]
 
 
 -- Exercício 02: Crie uma função projectEuler5 que retorna o primeiro número natural que retorna True para a função do
 -- exercício anterior. Pense em como reduzir o custo computacional.
-divisivel ::  Integer -> [Int]  -> Bool
-divisivel x l = foldl (||) True [ (rem x i) == 0 | i <- l ]
+divisivel ::  Integer -> [Integer]  -> Bool
+divisivel x l = foldl (||) False [ (rem i x) == 0 | i <- l ]
 
-removeDivisor :: [Int] -> [Int] -> Int -> [Int]
-removeDivisor ldst lsrc 0 = ldst
-removeDivisor ldst lsrc size
-    | 
+divisiveln :: Integer -> Integer -> Bool
+divisiveln x n = foldl (&&) True [ (rem x i) == 0 | i <- [1..n]]
 
-projectEuler5 :: Integer
-projectEuler5 = firstDiv 1
+-- mmc
+-- mdc
+-- fazendo os dois eu consigo char o primeiro número divisível por todos os números de 1 a n
+
+
+projectEuler5 :: Integer -> Integer
+projectEuler5 n = firstDiv 1
     where
-        firstDiv n 
-            | divisivel20 n == True = n
-            | otherwise = firstDiv (n+1)
+        firstDiv x 
+            | divisiveln x n == True = x
+            | otherwise = firstDiv (x+1)
 
 -- Exercício 03: Crie a lista de números de Fibonacci utilizando uma função geradora.
 
