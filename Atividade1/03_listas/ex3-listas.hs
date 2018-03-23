adee9af386043bb4f0b45bb9bf681ecec7ff49bb
Eduardo Pinhata <epinhata@Eduardos-MacBook-Pro.local>
2018-03-23 11:40:25 -0300

    Exercicio 2

--- Atividade1/03_listas/ex3-listas.hs
+++ Atividade1/03_listas/ex3-listas.hs
@@ -17,11 +17,31 @@ divisivel x l = foldl (||) False [ (rem i x) == 0 | i <- l ]
 divisiveln :: Integer -> Integer -> Bool
 divisiveln x n = foldl (&&) True [ (rem x i) == 0 | i <- [1..n]]
 
+(./?) :: [Integer] -> Integer -> Bool
+(./?) l x = foldl (||) False [ (rem i x) == 0 | i <- l]
+
 -- mmc
 -- mdc
 -- fazendo os dois eu consigo char o primeiro número divisível por todos os números de 1 a n
+(./) :: [Integer] -> Integer -> [Integer]
+(./) l x = saoDiv ++ naoSaoDiv
+    where
+        saoDiv = [ (div i x) | i <- l, (rem i x == 0 )] 
+        naoSaoDiv = [i | i <- l, (rem i x /= 0) ]
+
+isPrimo :: Integer -> Bool
+isPrimo x = not $ foldl (||) False [ (rem x i) == 0 | i <- [2..(x-1)] ]
 
 
+nextPrimo :: Integer -> Integer
+nextPrimo x = isPrimo' (x+1) 
+    where
+        isPrimo' x
+            | isPrimo x == True = x
+            | otherwise         = isPrimo' (x+1) 
+   
+
+-- Força Bruta
 projectEuler5 :: Integer -> Integer
 projectEuler5 n = firstDiv 1
     where
@@ -29,6 +49,17 @@ projectEuler5 n = firstDiv 1
             | divisiveln x n == True = x
             | otherwise = firstDiv (x+1)
 
+
+projectEuler6 :: Integer -> Integer
+projectEuler6 n = mdc [1..n] 1 2 
+        where 
+            mdc l m primo 
+                | ((sum l) == toInteger(length l)) || (primo > (sum l)) = m
+                | ( l ./? primo == True) = mdc (l ./ primo) (m*primo) primo 
+                | ( l ./? primo /= True) = mdc l m (nextPrimo primo) 
+
+
+            
 -- Exercício 03: Crie a lista de números de Fibonacci utilizando uma função geradora.
 
 -- Exercício 04: Utilizando a lista anterior, calcule a soma dos números de Fibonacci pares dos valores que não excedem 4.000.000. (Project Euler 2)
