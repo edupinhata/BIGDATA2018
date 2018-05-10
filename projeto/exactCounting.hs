058e56cb12a4b3634ed21ce3a1e7b24de9e62e6b
Eduardo <edupinhata@gmail.com>
2018-05-10 06:28:56 -0300

    Got database and read database

--- projeto/exactCounting.hs
+++ projeto/exactCounting.hs
@@ -1,6 +1,6 @@
 {-
 Module:	
-Description: 	Program that count k-cliques of a graph.
+Description:	Program that count k-cliques of a graph.
 Copyright:	(c) Eduardo Pinhata
 License:	GPL-3
 Maintainer:	edupinhata@gmail.com
@@ -8,13 +8,41 @@ Maintainer:	edupinhata@gmail.com
 
 -}
 
-import Vector.hs
+import Vector
+import Dados
+import Text.Read
+
+type Node		= (Integer, Integer)
+type HighNeigh	= [Integer]
+type Solution	= [(Node, HighNeigh)]
 
 
-module Main where
+parseFile :: String -> [Node]
+parseFile file = toNodes $ map parseLine (lines file)
+	where
+		parseLine l =  map toInteger (words l)
+		toInteger w = do
+						let maybeW = readMaybe w :: Maybe Integer
+						case maybeW of
+							Just w' -> w'
+							Nothing -> -1
+		toNodes n = map (\x -> (x!!0, x!!1)) n 
+
+
+-- Transform Nodes to Solution
+initSolution :: [Node] -> Solution
+initSolution rawData = emit $ prepare rawData
+	where 
+		parmap (\x ->  (x,[])) rawData
 
 
 
+
+main :: IO()
 main = do
-	print( [1,2] .+. [2,3] )
-
+	--file <- readFile "0.edges"
+	file <- readFile "3980.edges"
+	let
+		dataset = parseFile file
+	let solution = initSolution dataset
+	print( take 5 solution )		
