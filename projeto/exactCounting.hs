d48bd5fe5815967cf9b86ee9a16d92dce7f1d3fd
Eduardo Pinhata <pinhata@Eduardos-MacBook-Pro.local>
2018-05-09 21:36:46 -0300

    initial commit of project

---
+++ projeto/exactCounting.hs
@@ -0,0 +1,20 @@
+{-
+Module:	
+Description: 	Program that count k-cliques of a graph.
+Copyright:	(c) Eduardo Pinhata
+License:	GPL-3
+Maintainer:	edupinhata@gmail.com
+
+
+-}
+
+import Vector.hs
+
+
+module Main where
+
+
+
+main = do
+	print( [1,2] .+. [2,3] )
+
