5edd61f522fdae10e26f2f2f87eadef778193cd3
Eduardo <edupinhata@gmail.com>
2018-05-11 00:39:59 -0300

    Added more functions

--- projeto/exactCounting.hs
+++ projeto/exactCounting.hs
@@ -7,19 +7,33 @@ Maintainer: edupinhata@gmail.com
 
 -}
 
+module Main where
+
+import System.IO
+import System.Environment
+import Data.List.Split  --(chunksOf)
+
+import qualified Data.Map as Map
+
+import Prelude as P
 import Vector
 import Dados
 import Text.Read
 
-type Node = (Integer, Integer)
+numCks = 100
+
+
+-- Types
+type Node       = (Integer, Integer)
 type HighNeigh = [Integer]
-type Solution = [(Node, HighNeigh)]
+type Solution  = [(Node, HighNeigh)]
+type NeighSol  = [(Integer, [Integer])]
 
-
-parseFile :: String -> [Node]
+-- Format content read from file
+parseFile :: String ->  [Node]
 parseFile file = toNodes $ map parseLine (lines file)
     where
-        parseLine l =  map toInteger (words l)
+        parseLine l = map toInteger (words l)
         toInteger w = do
                         let maybeW = readMaybe w :: Maybe Integer
                         case maybeW of
@@ -29,20 +43,43 @@ parseFile file = toNodes $ map parseLine (lines file)
 
 
 -- Transform Nodes to Solution
-initSolution :: [Node] -> Solution
-initSolution rawData = emit $ prepare rawData
+initSolution :: ChunksOf Node -> Solution
+initSolution rawData = prepare rawData
     where 
-        parmap (\x -> (x,[]) ) rawData
+        prepare x = map (\x -> (x,[])) rawData
 
+
+-- Get Neighborhood of each node
+getNeigh :: ChunksOf [Node] -> NeighSol
+getNeigh rawData = mapReduceByKey separeSrcNode appendDst rawData
+    where
+        separeSrcNode x = (fst x, [snd x] )
+        appendDst x y = x++y
+
+
+-- Remove elements with Neighborhood bigger than k 
+filterK :: NeighSol -> Int -> NeighSol
+filterK neigh k = filter (\x -> length (snd x) >= k) neigh
+
+
+-- Implementation of symbol
+(<.) :: NeighSol -> NeighSol -> Bool
+(<.) x y 
+    | (P.length (snd $ x!!0) <= P.length (snd $ y!!0)) && x < y = True
+    | otherwise    = False
+
+
+-- Find High-Neighbor
+getHighNeigh :: NeighSol -> dict -> NeighSol
+getHighNeigh neigh dict = filter (\x ->  
+
+
+-- Create dictionary 
+makeDict :: ChunksOf [Node] -> Map.Map Integer [Integer]
+makeDict dataChunks = Map.fromList (getNeigh dataChunks)
 
 -- Get the High-Neighborhood for each node
-getHighNeigh :: [Node] -> [(Integer, [Integer])]
-getHighNeigh rawData = mapReduceByKey fromNode foldNeigh (filterGreater rawData)
-    where
-        fromNode x = (x!!0, [x!!1])
-        foldNeigh x,y = x++y
-        filterGreater x = filter 
-
+--getHighNeigh :: ChunksOf [Node] -> [(Integer, [Integer])]
 
 main :: IO()
 main = do
@@ -50,6 +87,8 @@ main = do
     file <- readFile "3980.edges"
     let
         dataset = parseFile file
-    let
-        solution = initSolution dataset
-    print( take 5 solution )
+        dataChunks = chunksOf numCks dataset
+        dict = makeDict dataChunks
+
+    print ( dict Map.! 3982 ) 
+
